e6a41b9595fc02351adc15e247202a3400b83068
Siddharth Chauhan <siddharthchauhan98@gmail.com>
2025-07-10 16:44:47 +0530

    remove logging and comments

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Use ["http://localhost:5173"] for stricter control
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- frontend/src/api/recipes.js
+++ frontend/src/api/recipes.js
@@ -3,16 +3,12 @@ import axios from "axios";
 const BASE_URL = "http://127.0.0.1:8000/api/recipes";
 
 export const getRecipes = async (page = 1, limit = 15) => {
-  console.log(`Making get recipes API call at ${BASE_URL}?page=${page}&limit=${limit}`);
   const res = await axios.get(`${BASE_URL}?page=${page}&limit=${limit}`);
-  console.log("Get Recipes API response:", res);
   return res.data;
 };
 
 export const searchRecipes = async (filters, page = 1, limit = 15) => {
   const params = new URLSearchParams(filters).toString();
-  console.log(`Making search API call at ${BASE_URL}/search?${params}`);
   const res = await axios.get(`${BASE_URL}/search?${params}&page=${page}&limit=${limit}`);
-  console.log("Search API response:", res);
   return res.data;
 };
